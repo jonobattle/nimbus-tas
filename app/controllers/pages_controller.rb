58225ab29acd52bb42ca5ab4be797d5ea1b4c362
Jono Battle <jono.battle@gmail.com>
2013-03-19 23:06:07 +1000

    more

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -41,6 +41,23 @@ class PagesController < ApplicationController
 
   end
 
+  def show_sub_sub_page
+
+    # Grab the page slug from the params and search for that page
+    @site = Site.first()
+
+    if @site
+
+      @root_page = @site.pages.where(:slug => params[:page_slug]).first
+      @page = @root_page.pages.where(:slug => params[:sub_page_slug]).first
+      @sub_page = @page.pages.where(:slug => params[:sub_sub_page_slug]).first
+
+      render :template => "templates/custom-tas/views/" + @page.template.to_s
+
+    end
+
+  end  
+
 
   private
 
